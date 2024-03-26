583cc42dc9355bfbdc5f1aab9f3c47f43d21b49b
See-Its-Manish <seeitsmanish@gmail.com>
2024-03-27 01:25:28 +0530

    Initialized app

---
+++ app/components/Appbar/Appbar.js
@@ -0,0 +1,6 @@
+"use client"
+export default function Appbar() {
+  return (
+    <div>Appbar</div>
+  )
+}
---
+++ app/components/Appbar/page.js
@@ -0,0 +1,13 @@
+"use client"
+import Appba from "./Appbar"
+export default function page() {
+    return (
+        <Appbar 
+            primaryBackground
+            secondaryBackground
+            primaryText
+            secondaryText
+            hoverText
+        />
+    )
+    }
