7a0bc3f663178bbb55f083b22652b8773931a213
See-Its-Manish <seeitsmanish@gmail.com>
2024-03-28 00:20:17 +0530

    Made Some Parts of Appbar

--- app/components/Appbar/Appbar.js
+++ app/components/Appbar/Appbar.js
@@ -1,6 +1,43 @@
-"use client"
-export default function Appbar() {
+"use client";
+import LogoImage from "@/public/logo-image.png";
+export default function Appbar({ appConfig }) {
+  const { BackgroundColor, TextColor, ButtonColor } = appConfig;
   return (
-    <div>Appbar</div>
-  )
+    <nav
+      className={`w-full bg-${BackgroundColor.primary} h-16 p-2 flex justify-between items-center shadow-md`}
+    >
+      <div className="logo-container flex items-center gap-1">
+        <img
+          src={LogoImage.src}
+          alt="Logo"
+          className="h-12 w-12 md:h-9 md:w-9"
+        />
+        <span className="text-2xl font-lg hidden md:inline">Logo</span>
+      </div>
+
+      <div className="nav-links hidden md:block">
+        <ul className={`flex text-xl gap-5`}>
+          <li className="hover:text-gray-400 hover:cursor-pointer">Home</li>
+          <li className="hover:text-gray-400 hover:cursor-pointer">Services</li>
+          <li className="hover:text-gray-400 hover:cursor-pointer">
+            Subscription
+          </li>
+        </ul>
+      </div>
+      <div className="auth-buttons text-lg text hidden md:inline md:flex md:gap-3 ">
+        <button
+          className={`border-2 ${ButtonColor.border} p-1 rounded-[40px] w-24 text-blue-600 hover:bg-slate-100 ease-in-out duration-300 `}
+        >
+          Login
+        </button>
+        <button
+          className={`${ButtonColor.primary} p-1 rounded-[40px] w-24 text-white hover:bg-blue-800 ease-in-out duration-300`}
+        >
+          SignUp
+        </button>
+      </div>
+
+      <div className="mobile-side-bar md:hidden"></div>
+    </nav>
+  );
 }
--- app/components/Appbar/page.js
+++ app/components/Appbar/page.js
@@ -1,13 +1,32 @@
 "use client"
-import Appba from "./Appbar"
+import Appbar from "./Appbar"
+
+const BackgroundColor = {
+    primary: 'bg-blue-100',
+    secondary: 'bg-blue-100'
+}
+
+const TextColor = {
+    primary: 'text-black',
+    secondary: 'text-gray-400',
+}
+
+const ButtonColor = {
+    primary: 'bg-blue-600',
+    hover: 'bg-blue-800',
+    border: 'border-blue-600',
+}
+
+const appConfig = {
+    BackgroundColor,
+    TextColor,
+    ButtonColor,
+}
+
 export default function page() {
     return (
         <Appbar 
-            primaryBackground
-            secondaryBackground
-            primaryText
-            secondaryText
-            hoverText
+            appConfig = {appConfig}
         />
     )
-    }
+}
